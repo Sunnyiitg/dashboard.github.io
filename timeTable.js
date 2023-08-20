bc1770141337e8114e03eaf352e106ad55d1fc31
Sunnyiitg <sunnyfromiitg@gmail.com>
2023-08-21 03:57:42 +0530

    first commit

---
+++ timeTable.js
@@ -0,0 +1,138 @@
+const Sunday =[
+    {   
+        time: 'Weekend',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '10-12 AM',
+        roomNumber: 'G1',
+        subject: 'LAB - NA',
+        type: 'LAB'
+    },
+    {   
+        time: '2-3 PM',
+        roomNumber: 'G1',
+        subject: 'DBMS',
+        type: 'Lecture'
+    },
+    {   
+        time: '4-5 PM',
+        roomNumber: 'G1',
+        subject: 'Functional Analysis',
+        type: 'Lecture'
+    },
+    {   
+        time: '5-6 PM',
+        roomNumber: 'G1',
+        subject: 'TOC',
+        type: 'Lecture'
+    }
+]
+const Tuesday =[
+    {   
+        time: '3-4 PM',
+        roomNumber: 'G1',
+        subject: 'Functional Analysis',
+        type: 'Lecture'
+    },
+    {   
+        time: '4-5 PM',
+        roomNumber: 'G1',
+        subject: 'TOC',
+        type: 'Lecture'
+    },
+    {   
+        time: '5-6 PM',
+        roomNumber: 'G1',
+        subject: 'Numerical Analysis',
+        type: 'Lecture'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '12-1 PM',
+        roomNumber: 'L1',
+        subject: 'Mathematical Finance',
+        type: 'LAB'
+    },
+    {   
+        time: '2-3 PM',
+        roomNumber: 'G1',
+        subject: 'Functional Analysis',
+        type: 'Lecture'
+    },
+    {   
+        time: '3-4 PM',
+        roomNumber: 'G1',
+        subject: 'TOC',
+        type: 'Lecture'
+    },
+    {   
+        time: '4-5 PM',
+        roomNumber: 'G1',
+        subject: 'Numerical Analysis',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '12-1 PM',
+        roomNumber: 'L1',
+        subject: 'Mathematical Finance',
+        type: 'LAB'
+    },
+    {   
+        time: '2-3 PM',
+        roomNumber: 'G1',
+        subject: 'TOC',
+        type: 'Lecture'
+    },
+    {   
+        time: '3-4 PM',
+        roomNumber: 'G1',
+        subject: 'Numerical Analysis',
+        type: 'Lecture'
+    },
+    {   
+        time: '4-5 PM',
+        roomNumber: 'G1',
+        subject: 'DBMS',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '9-12 AM',
+        roomNumber: 'LAB',
+        subject: 'DBMS - LAB',
+        type: 'LAB'
+    },
+    {   
+        time: '12-1 PM',
+        roomNumber: 'L1',
+        subject: 'Mathematical Finance',
+        type: 'LAB'
+    },
+    {   
+        time: '5-6 PM',
+        roomNumber: 'G1',
+        subject: 'Functional Analysis',
+        type: 'Lecture'
+    }
+]
+
+const Saturday =[
+    {   
+        time: 'Weekend',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
